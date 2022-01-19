6deb6b64cacc9f840bcc6be3235ee5fd54c788c3
Navraj <navrajkhanal61@gmail.com>
2022-01-19 22:39:29 +0545

    Added all the files

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask,request,jsonify
+app = Flask(__name__)
+import engine as en
+
+
+@app.route('/api/v1/getAnalysis/', methods=['GET','POST'])
+def getMessage():
+    # handle the POST request
+    content = request.json
+    message = content["message"]
+    final_result = en.final_analysis(message)
+    return jsonify({"data":final_result})
+
+
+
